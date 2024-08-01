c6888f89bd60200340ac45037e7570ed3ab359e5
Mark Coafield <46470837+MissedTheMark@users.noreply.github.com>
2024-08-01 16:31:13 +1000

    Update comment

--- pkg/migration.go
+++ pkg/migration.go
@@ -16,7 +16,7 @@ var semVerRegEx = "(?P<major>0|[1-9]\\d*)\\.(?P<minor>0|[1-9]\\d*)\\.(?P<patch>0
 
 func NewMigration(fileName string) (*Migration, error) {
 
-	// Get version string from file name, eg migration-v1.0.0-v1.0.1.yaml
+	// Get version string from file name, eg '1.0.0-1.0.1.yaml'
 	pattern := fmt.Sprintf(`(?P<fromVersion>%s)\-(?P<toVersion>%s)\.yaml`, semVerRegEx, semVerRegEx)
 	re := regexp.MustCompile(pattern)
 
