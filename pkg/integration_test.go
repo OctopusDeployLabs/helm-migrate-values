9adefb7429aaa6fcea6c123519e2e1f748190712
Mark Coafield <46470837+MissedTheMark@users.noreply.github.com>
2024-09-20 15:29:51 +1000

    Missed some

--- pkg/integration_test.go
+++ pkg/integration_test.go
@@ -1,9 +1,9 @@
 package pkg
 
 import (
+	"github.com/octopusdeploylabs/helm-migrate-values/internal"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"helm-migrate-values/internal"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/release"
